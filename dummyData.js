8232f687a547f9de9c6e825fe3812c2fe154ee6d
g-saint97 <georgesainthilaire@Georges-iMac.local>
2023-12-06 14:31:54 -0500

    first commit

---
+++ dummyData.js
@@ -0,0 +1,28 @@
+const jsonData = `{
+    "users": [
+      {
+        "id": 1,
+        "name": "John Doe",
+        "email": "john@example.com",
+        "address": {
+          "city": "New York",
+          "zipcode": "10001"
+        }
+      }
+    ],
+    "products": [
+      {
+        "id": 101,
+        "name": "Laptop",
+        "price": 999.99
+      },
+      {
+        "id": 102,
+        "name": "Smartphone",
+        "price": 499.99
+      }
+    ]
+  }`;
+  
+console.log(JSON.parse(jsonData));
+  
